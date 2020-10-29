1faadd8b8109fb032a80162e77bdd8aa2feeca04
Japhet Diaz <31853144+JaphetD@users.noreply.github.com>
2020-10-29 18:09:06 -0400

    update to deletion

--- ProjectOne/app/app.js
+++ ProjectOne/app/app.js
@@ -122,26 +122,82 @@ function onTrailClick8() {
 
 
 
+// let firstRedPiece = document.getElementById("redPieceOne");
+// let secondRedPiece = document.getElementById("redPieceTwo");
+// let thirdRedPiece = document.getElementById("redPieceThree");
+// let fourthRedPiece = document.getElementById("redPieceFour");
+// let fifthRedPiece = document.getElementById("redPieceFive");
+// let sixthRedPiece = document.getElementById("redPieceSix");
+// let seventhRedPiece = document.getElementById("redPieceSeven");
+// let eighthRedPiece = document.getElementById("redPieceEight");
+// let ninthRedPiece = document.getElementById("redPieceNine");
+// let tenthRedPiece = document.getElementById("redPieceTen");
+// let elevenRedPiece = document.getElementById("redPieceEleven");
+// let twelveRedPiece = document.getElementById("redPieceTwelve");
 
 
 
 
-//Double click event listener 
+
+//Double click event listener for deletion(red pieces)
 firstRedPiece.addEventListener("dblclick", ondblclick);
 secondRedPiece.addEventListener("dblclick", ondblclick2);
-firstWhitePiece.addEventListener("dblclick", ondblclick3);
+thirdRedPiece.addEventListener("dblclick", ondblclick4);
+fourthRedPiece.addEventListener("dblclick", ondblclick5);
+fifthRedPiece.addEventListener("dblclick", ondblclick6);
+sixthRedPiece.addEventListener("dblclick", ondblclick7);
+seventhRedPiece.addEventListener("dblclick", ondblclick8);
+eighthRedPiece.addEventListener("dblclick", ondblclick9);
+ninthRedPiece.addEventListener("dblclick", ondblclick10);
+tenthRedPiece.addEventListener("dblclick", ondblclick11);
+elevenRedPiece.addEventListener("dblclick", ondblclick12);
+twelveRedPiece.addEventListener("dblclick", ondblclick13);
 
-//Double click will remove the piece 
+
+//Double click deletion listeners will remove the piece 
 function ondblclick() {
     TweenLite.to(firstRedPiece, 1,{autoAlpha: 0,display: "none"});
 }
-
 function ondblclick2() {
     TweenLite.to(secondRedPiece, 1,{autoAlpha: 0,display: "none"});
 }
+function ondblclick4() {
+    TweenLite.to(thirdRedPiece, 1,{autoAlpha: 0,display: "none"});
+}
+function ondblclick5() {
+    TweenLite.to(fourthRedPiece, 1,{autoAlpha: 0,display: "none"});
+}
+function ondblclick6() {
+    TweenLite.to(fifthRedPiece, 1,{autoAlpha: 0,display: "none"});
+}
+function ondblclick7() {
+    TweenLite.to(sixthRedPiece, 1,{autoAlpha: 0,display: "none"});
+}
+function ondblclick8() {
+    TweenLite.to(seventhRedPiece, 1,{autoAlpha: 0,display: "none"});
+}
+function ondblclick9() {
+    TweenLite.to(eighthRedPiece, 1,{autoAlpha: 0,display: "none"});
+}
+function ondblclick10() {
+    TweenLite.to(ninthRedPiece, 1,{autoAlpha: 0,display: "none"});
+}
+function ondblclick11() {
+    TweenLite.to(tenthRedPiece, 1,{autoAlpha: 0,display: "none"});
+}
+function ondblclick12() {
+    TweenLite.to(elevenRedPiece, 1,{autoAlpha: 0,display: "none"});
+}
+function ondblclick13() {
+    TweenLite.to(twelveRedPiece, 1,{autoAlpha: 0,display: "none"});
+}
+
+
+//Double click listeners for deletion for white pieces 
+firstWhitePiece.addEventListener("dblclick", ondblclick3);
+
 
 function ondblclick3() {
     TweenLite.to(firstWhitePiece, 1,{autoAlpha: 0,display: "none"});
 }
-
 
